843832e4e28f621a61bdedfc8e8d31a47d9f60e4
Sean Bristol-Lee <seanbristol81@gmail.com>
2024-07-31 04:01:51 -0700

    add search by alphabet

--- lib/health_modules/by_alphabet.dart
+++ lib/health_modules/by_alphabet.dart
@@ -1,11 +1,66 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'module_by_alphabet.dart';
 
 class ByAlphabet extends StatefulWidget {
   @override
   _ByAlphabetState createState() => _ByAlphabetState();
 }
 
+Future<List<Album>> fetchAlbums() async {
+  final response = await http.get(Uri.parse('https://obrpqbo4eb.execute-api.us-west-2.amazonaws.com/api/letters'));
+
+  if (response.statusCode == 200) {
+    print("This is the response ${response.body}");
+    Iterable jsonResponse = json.decode(response.body);
+    List<Album> albums = jsonResponse.map((album) => Album.fromJson(album)).toList();
+    albums.sort((a, b) => a.name.compareTo(b.name));
+    return albums;
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final String description;
+  final String id;
+  final String name;
+
+  Album({
+    required this.description,
+    required this.id,
+    required this.name,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        "description": String description,
+        "id": String id,
+        "name": String name,
+      } => Album(
+        description: description,
+        id: id,
+        name: name,
+      ),
+      _ => throw Exception('Failed to load album'),
+    };
+  }
+}
+
 class _ByAlphabetState extends State<ByAlphabet> {
+  late Future<List<Album>> futureAlbums;
+  late List<String> albumNames = [];
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbums = fetchAlbums();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +73,8 @@ class _ByAlphabetState extends State<ByAlphabet> {
             Text("Search by"),
             Text("Alphabet"),
             Container(
-              height: 100,
-              width: 100,
+              height: 400,
+              width: 400,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
@@ -28,6 +83,50 @@ class _ByAlphabetState extends State<ByAlphabet> {
                   width: 2,
                 ),
               ),
+              child: FutureBuilder<List<Album>>(
+                future: futureAlbums,
+                builder: (context, snapshot) {
+                  print("THis is the snapshot ${snapshot.data}");
+                  if (snapshot.hasData) {
+                    albumNames = snapshot.data!.map((album) => album.name).toList();
+                    return GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        childAspectRatio: 3,
+                      ),
+                      itemCount: albumNames.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            print("Album index ${albumNames[index]} was tapped");
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ModuleByAlphabet(letter: albumNames[index])),
+                            );
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Text(
+                              albumNames[index],
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+            ),
             ),
           ],
         ),
---
+++ lib/health_modules/module_by_alphabet.dart
@@ -0,0 +1,151 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ModuleByAlphabet extends StatefulWidget {
+  final String letter;
+
+  ModuleByAlphabet({required this.letter});
+
+  @override
+  _ModuleByAlphabetState createState() => _ModuleByAlphabetState();
+}
+
+
+
+class Modules {
+  String? name;
+  //String? description;
+  //String? topics;
+  //String? version;
+  String? downloadLink;
+  //String? launchFile;
+  //String? packageSize;
+  //String? letters;
+  //String? credits;
+  //String? module_name;
+  //String? id;
+
+
+  Modules({
+    this.name,
+    //this.description,
+    //this.topics,
+    //this.version,
+    this.downloadLink,
+    //this.launchFile,
+    //this.packageSize,
+    //this.letters,
+    //this.credits,
+    //this.module_name,
+    //this.id,
+  });
+
+  Modules.fromJson(Map<String, dynamic> json)
+    : name = json['name'] as String,
+      //description = json['description'] as String,
+      //topics = json['topics'] as String,
+      //version = json['version'] as String,
+      downloadLink = json['downloadLink'] as String;
+      //launchFile = json['launchFile'] as String,
+      //packageSize = json['packageSize'] as String,
+      //letters = json['letters'] as String;
+      //credits = json['credits'] as String,
+      //module_name = json['module_name'] as String,
+      //id = json['id'] as String;
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    //'description': description,
+    //'topics': topics,
+    //'version': version,
+    'downloadLink': downloadLink,
+    //'launchFile': launchFile,
+    //'packageSize': packageSize,
+    //'letters': letters,
+    //'credits': credits,
+    //'module_name': module_name,
+    //'id': id,
+  };
+}
+
+class _ModuleByAlphabetState extends State<ModuleByAlphabet> {
+  late Future<List<Modules>> futureModules;
+  late List<Modules> moduleData = [];
+
+  Future<List<Modules>> getModules() async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://obrpqbo4eb.execute-api.us-west-2.amazonaws.com/api/modules'));
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        moduleData = data.map<Modules>((e) => Modules.fromJson(e)).toList();
+        debugPrint("Module Data: ${moduleData.length}");
+        return moduleData;
+      } else {
+        debugPrint("Failed to load modules");
+      }
+      return moduleData;
+    } catch (e) {
+      debugPrint("$e");
+    }
+    return moduleData;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureModules = getModules();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("By Alphabet ${widget.letter}"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Search by"),
+            Text("Alphabet: ${widget.letter}"),
+            Container(
+              height: 400,
+              width: 400,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 2,
+                ),
+              ),
+              child: FutureBuilder<List<Modules>>(
+                future: getModules(),
+                builder: (context, snapshot) {
+                  print("THis is the snapshot ${snapshot.data}");
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: moduleData.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(moduleData[index].name!),
+                          subtitle: Text(moduleData[index].downloadLink!),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
